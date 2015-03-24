01c7d9ee23791228c7197ba05e40914686eefc79
Damien Monni <monni.damien@gmail.com>
2015-03-24 20:52:57 +0100

    Add the compFilter files

---
+++ 02 - Complementary filter/monni_compFilter.c
@@ -0,0 +1,8 @@
+/*
+ * monni_compFilter.c
+ *
+ * Created: 24/03/2015 20:51:56
+ *  Author: Damien MONNI
+ */ 
+
+#include "monni_compFilter.h"
---
+++ 02 - Complementary filter/monni_compFilter.h
@@ -0,0 +1,16 @@
+/*
+ * monni_compFilter.h
+ *
+ * Created: 24/03/2015 20:51:43
+ *  Author: Damien MONNI
+ */ 
+
+
+#ifndef MONNI_COMPFILTER_H_
+#define MONNI_COMPFILTER_H_
+
+
+
+
+
+#endif /* MONNI_COMPFILTER_H_ */
