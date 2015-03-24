25c452aafad58a159b13d6617ac5a94f841cab48
Damien Monni <monni.damien@gmail.com>
2015-03-24 21:35:59 +0100

    Add interrupt init

--- 02 - Complementary filter/monni_compFilter.c
+++ 02 - Complementary filter/monni_compFilter.c
@@ -16,9 +16,20 @@ static const int16_t GRAVITY = 4096; //Raw value of gravity. 8g max on 16 signed
 
 
 /************************************************************************/
+/* Complementary filter init                                                                     */
+/************************************************************************/
+void mCompInit(){
+	//8-bits Timer 0 configuration
+	TCCR0A = 0; //Normal mode
+	TCCR0B |= 1<<CS01; //Prescaling /8 => 1 tick every us
+	TIMSK0 |= 1<<TOIE0; //Interrupt on overflow (every 256us)
+}
+
+
+/************************************************************************/
 /* GYRO INIT                                                            */
 /************************************************************************/
-void gyroInit(){
+void mCompGyroInit(){
 	
 	while(twiWriteOneByte(gyroAdd, 0x20, 0x0F) == 0); //CTRL1 => 100Hz - Low Pass Filter 25Hz
 	while(twiWriteOneByte(gyroAdd, 0x21, 0x00) == 0); //CTRL2 => High pass filter 8Hz to remove some drift
