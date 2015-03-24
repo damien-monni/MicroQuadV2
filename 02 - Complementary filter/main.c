23996c08a2659dc650ae1db262f215604307ac56
Damien Monni <monni.damien@gmail.com>
2015-03-24 20:50:43 +0100

    Initialisation process with a LED

--- 02 - Complementary filter/main.c
+++ 02 - Complementary filter/main.c
@@ -2,16 +2,39 @@
  * _02___Complementary_filter.c
  *
  * Created: 24/03/2015 20:44:00
- *  Author: monni_000
+ *  Author: Damien MONNI
  */ 
 
+#define F_CPU 8000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+/************************************************************************/
+/* PROTOTYPES                                                           */
+/************************************************************************/
+void initLed();
 
 int main(void)
 {
     while(1)
     {
-        //TODO:: Please write your application code 
+        /************************************************************************/
+        /* LED INITIALISATION                                                   */
+        /************************************************************************/
+		initLed();
+		
+		
     }
+}
+
+
+void initLed(){
+	DDRD |= 1<<DDD0; //PORTD0 as output
+	PORTD |= 1<<PORTD0;
+	
+	PORTD |= 1<<PORTD0; //Turn on LED on PORTD0
+	_delay_ms(1500); //Wait 1.5s
+	PORTD &= ~(1<<PORTD0); //Turn off LED on PORTD0
+	_delay_ms(1500); //Wait 1.5s
 }
