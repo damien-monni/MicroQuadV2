5103fc24acc51818f2d4a265d35d4482d38c74b4
Damien Monni <monni.damien@gmail.com>
2015-03-24 20:45:09 +0100

    Create a new project: complementary filter

---
+++ 02 - Complementary filter/main.c
@@ -0,0 +1,17 @@
+/*
+ * _02___Complementary_filter.c
+ *
+ * Created: 24/03/2015 20:44:00
+ *  Author: monni_000
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
